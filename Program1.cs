abd502a59bd97b4f8a79fdaefadc0e3bd8fd3ec7
donggeunchoi <cdg01063149045@gmail.com>
2025-04-14 16:23:21 +0900

    Update Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -67,3 +67,38 @@ double BMI = Kg / BMIm;
 Console.WriteLine(BMIm);
 
 Console.WriteLine($"당신의 BMI : {BMI}");
+
+// 게임 시작화면 만들기.
+
+Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
+Console.WriteLine();
+Console.WriteLine("1. 상태 보기");
+Console.WriteLine("2. 인벤토리");
+Console.WriteLine("3. 상점");
+Console.WriteLine();
+Console.WriteLine("원하시는 행동을 입력해주세요.");
+int gameloby = int.Parse(Console.ReadLine());
+
+if(gameloby <= 0 && gameloby > 3 )
+{
+Console.WriteLine("잘못된 입력입니다.");
+}
+
+else
+{
+    if(gameloby == 1)
+{
+ Console.WriteLine("상태보기 창으로 입장하셨습니다.");
+
+}
+else if( gameloby == 2)
+{
+    Console.WriteLine("인벤토리에 입장하셨습니다.");
+}
+else
+{
+ Console.WriteLine("상점으로 입장하셨습니다.");
+}
+
+}
